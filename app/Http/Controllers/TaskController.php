cc9a61220c89582c5ba1e13d39e7dfa248a81c01
hirokatsuiino <letterbox6212>
2020-06-08 17:44:11 +0900

    [ADD]タスク機能

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 //use App\Folder;
 use App\Folder;
+use App\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -13,12 +14,27 @@ class TaskController extends Controller
 //        return "Hello world";
 //    }
     {
+        // すべてのフォルダを取得する
         $folders = Folder::all();
+
+        // 選ばれたフォルダを取得する
+        $current_folder = Folder::find($id);
+
+        // 選ばれたフォルダに紐づくタスクを取得する
+//        $tasks = Task::where('folder_id', $current_folder->id)->get();
+        // 選ばれたフォルダに紐づくタスクを取得する
+        $tasks = $current_folder->tasks()->get(); // ★
 
         return view('tasks/index', [
             'folders' => $folders,
-            'current_folder_id' => $id,
+            'current_folder_id' => $current_folder->id,
+            'tasks' => $tasks,
         ]);
+
+//        return view('tasks/index', [
+//            'folders' => $folders,
+//            'current_folder_id' => $id,
+//        ]);
     }
 
 
